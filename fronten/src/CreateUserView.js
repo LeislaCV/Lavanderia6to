cc3b7e072faff724d5146a3039c1971d2d4129b8
LeislaCV <you@example.com>
2025-06-06 23:55:26 -0600

    Hice lo que pude:(

--- fronten/src/CreateUserView.js
+++ fronten/src/CreateUserView.js
@@ -1,9 +1,30 @@
 
 import { Container, Card, Form, Button} from "react-bootstrap";
-
+import axios from 'axios';
+import React, { useState } from 'react';
 
 function CreateUserView() {
 
+    const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async () => {
+    try {
+      await axios.post("http://localhost:4000/users", {
+        name,
+        email,
+        password,
+      });
+      alert("✅ Usuario creado correctamente");
+      setName("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.error(error);
+      alert("❌ Error al crear usuario");
+    }
+  };
   return (
 
     <Container className="justify-content-center align-items-center" >
@@ -16,21 +37,24 @@ function CreateUserView() {
               <Form>
                 <Form.Group className="mb-3">
                   <Form.Label>📛 Nombre:</Form.Label>
-                  <Form.Control placeholder=" Ingresa tu nombre" />
+                  <Form.Control placeholder=" Ingresa tu nombre" value={name}
+                onChange={(e) => setName(e.target.value)}/>
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                   <Form.Label>📧 Email:</Form.Label>
-                  <Form.Control placeholder="Ingresa tu email" />
+                  <Form.Control placeholder="Ingresa tu email"  value={email}
+                onChange={(e) => setEmail(e.target.value)}/>
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                   <Form.Label>🔒 Contraseña:</Form.Label>
-                  <Form.Control placeholder="Ingresa tu contraseña " />
+                  <Form.Control placeholder="Ingresa tu contraseña " type="password" value={password}
+                onChange={(e) => setPassword(e.target.value)} />
                 </Form.Group>
 
               
-                  <Button className="text-center" variant="primary">🧑‍💻 Crear Cliente</Button>
+                  <Button className="text-center" type="submit" onClick={() => onSubmit()} variant="primary">🧑‍💻 Crear Usuario</Button>
                 
               </Form>
             </Card.Body>
@@ -41,3 +65,5 @@ function CreateUserView() {
 }
 
 export default CreateUserView;
+
+
